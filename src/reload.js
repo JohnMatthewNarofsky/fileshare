edba651f91643c873b2ec1a006516d058b96f096
John Matthew Narofsky <7ytd765789@gmail.com>
2020-09-20 15:16:56 -0500

    make auto reload script attempt to reconnect when connection is lost

--- src/reload.js
+++ src/reload.js
@@ -1,5 +1,29 @@
-const socket = new WebSocket("ws://localhost:9000");
+const address = "ws://localhost:9000";
+var socket = new WebSocket(address);
+function milliseconds(t) {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve('resolved');
+		}, t);
+	});
+}
 
-socket.addEventListener('message', function(event) {
+function on_message(event) {
 	location.reload();
-});
+}
+function on_error(event) {}
+
+async function on_close(event) {
+	console.log("Reload server connection closed. Retrying...");
+	socket.close();
+	socket = new WebSocket(address);
+	init_socket(socket);
+}
+
+function init_socket(socket) {
+	socket.addEventListener('close', on_close);
+	socket.addEventListener('message', on_message);
+	socket.addEventListener('error', on_error);	
+}
+
+init_socket(socket);
